74df4d121a9a20edc633e83f254c26a9ad304b94
Bharat74 <jayabharat286@gmail.com>
2021-12-15 11:35:23 +0530

    This is my first gitbash

---
+++ src/test/java/GITbashSDET3/SampleTest.java
@@ -0,0 +1,11 @@
+package GITbashSDET3;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("BOTH ARE NOT SAME");
+
+	}
+
+}
